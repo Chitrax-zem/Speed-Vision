374ed2613454b30152a0b62dfab497131cd07bda
Chitransh Nigam <chitranshnigam2000@gmail.com>
2026-05-25 13:42:51 +0530

    fix vercel cors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -23,6 +23,7 @@ class Settings(BaseSettings):
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://speed-vision.vercel.app",
     ]
 
     UPLOAD_DIR: str = "/tmp/uploads"
